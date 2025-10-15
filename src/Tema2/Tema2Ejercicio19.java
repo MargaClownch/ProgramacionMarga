309908eba263dd829cca97316bcd563ee1e9cc35
Marga <iulcaz@alu.edu.gva.es>
2025-10-15 13:25:22 +0200

    Ejercicio19

---
+++ src/Tema2/Tema2Ejercicio19.java
@@ -0,0 +1,14 @@
+package Tema2;
+//Escribe un programa que muestra por consola todos los números pares
+//entre 1 y 1000.
+
+public class Tema2Ejercicio19 {
+    public static void main(String[] args){
+        int contador = 1;
+        for (int i = 1; i<=1000; i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
