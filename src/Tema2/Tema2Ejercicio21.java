5769c04b1ed768a7c0e622161a1146772bfb4ae8
Marga <iulcaz@alu.edu.gva.es>
2025-10-16 10:28:38 +0200

    Ejercicio21

---
+++ src/Tema2/Tema2Ejercicio21.java
@@ -0,0 +1,14 @@
+package Tema2;
+//Escribe un programa que calcula la suma de todos los números pares
+//entre 1 y 1000 y la muestra por consola.
+public class Tema2Ejercicio21 {
+    public static void main(String[] args) {
+        int contador = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                contador = contador + i;
+            }
+        }
+        System.out.println(contador);
+    }
+}
