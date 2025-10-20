8f57444db16bbc565ae4b656109f93753cc638f2
Marga <iulcaz@alu.edu.gva.es>
2025-10-20 09:24:20 +0200

    Ejercicio26

---
+++ src/Tema2/Tema2Ejercicio26.java
@@ -0,0 +1,27 @@
+package Tema2;
+import java.util.Scanner;
+//Haz un programa que pida un número entero por consola e indica si es
+//primo o no. Un número primo sólo es divisible por él mismo y por la
+//unidad.
+public class Tema2Ejercicio26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número entero: ");
+        int num = sc.nextInt();
+        boolean esPrimo = true;
+        if (num <= 1) {
+            esPrimo = false;
+        } else
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                }
+                if (esPrimo) {
+                    System.out.println(num + " es un número primo.");
+                } else {
+                    System.out.println(num + " no es un número primo.");
+                }
+            }
+    }
+}
+
