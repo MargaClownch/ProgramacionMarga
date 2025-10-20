e06b5b2740af26a2dbe5592038cd77bb89a68b43
Marga <iulcaz@alu.edu.gva.es>
2025-10-20 08:41:58 +0200

    Ejercicio24

--- src/Tema2/Tema2Ejercicio24.java
+++ src/Tema2/Tema2Ejercicio24.java
@@ -1,4 +1,27 @@
 package Tema2;
-
+import java.util.Scanner;
+//Realiza un programa que permite a un profesor introducir la nota de un
+//número indefinido de alumnos y le indica cuántos suspendidos o
+//aprobados hay.
 public class Tema2Ejercicio24 {
-}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int apro=0;
+        int sus=0;
+        int nota;
+        System.out.println("Introduce la nota del alumno o un número negativo para salir del programa: ");
+
+        do {
+            nota = sc.nextInt();
+            if (nota>=5){
+                apro++;
+            }else
+                sus++;
+        }
+        while (nota>=0);
+
+        System.out.println("Hay "+ apro + "aprobados!");
+        System.out.println("Hay "+ (sus-1) + "suspendidos...");
+        }
+    }
+
