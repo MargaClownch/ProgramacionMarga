2e4e2039dc97c79f122126412f2a4a36f0921056
Marga <iulcaz@alu.edu.gva.es>
2025-10-20 10:14:49 +0200

    Ejercicio29

---
+++ src/Tema2/Tema2Ejercicio29.java
@@ -0,0 +1,25 @@
+package Tema2;
+import java.util.Scanner;
+import java.util.Random;
+//El número secreto”. Programa un juego en el que se calcula
+//aleatoriamente un número de 1 a 100. El usuario debe adivinarlo. Cada
+//vez que el usuario pone un número el programa le indicará si lo ha
+//acertado, si el número secreto es mayor o si es menor. Termina cuando
+//el usuario acierta el número.
+public class Tema2Ejercicio29 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int randomInt = random.nextInt(100);
+        int num=0;
+        System.out.println("Introduce el número correcto!: ");
+
+        while (num!=randomInt){
+            num = sc.nextInt();
+           if (num!=randomInt){
+               System.out.println("Has fallado perro");
+           }else
+               System.out.println("Has ganado!!!");
+        }
+    }
+}
