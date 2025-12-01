c51e7f071321df81de3a79b5a0f9b59b8cc7be0c
Marga <iulcaz@alu.edu.gva.es>
2025-12-01 10:12:12 +0100

    Ejercicios

---
+++ src/Tema3/Array/MyArray.java
@@ -0,0 +1,34 @@
+package Tema3.Array;
+
+public class MyArray {
+    public static void imprimirArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static int arrayMax(int arr []){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static int arrayMin(int arr []){
+        int min = arr[0];
+        for(int i = 0 ; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    public static int arrayAverage(int arr []){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum/arr.length;
+    }
+}
