893abddbc2d59ae63cca1896c782fb44160fb66f
Marga <iulcaz@alu.edu.gva.es>
2026-01-14 13:37:53 +0100

    Ejercicios

--- src/Tema4/Hero.java
+++ src/Tema4/Hero.java
@@ -1,6 +1,25 @@
 package Tema4;
 
 public class Hero {
+    // Atributos
     private String name;
+    private int level;
+    private int health;
+    private int maxHealth;
+    private int experience;
+    private int attack;
+    private int defense;
+    // Constructores
+    public Hero(){
 
+    }
+    public Hero(String name, int level, int health, int maxHealth, int experience, int attack, int defense) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.experience = experience;
+        this.attack = attack;
+        this.defense = defense;
+    }
 }
