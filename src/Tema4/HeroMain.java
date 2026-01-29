bd51aabc76728a32684a094341a60fe33118ba9f
Marga <iulcaz@alu.edu.gva.es>
2026-01-29 09:54:11 +0100

    Ejercicios

--- src/Tema4/HeroMain.java
+++ src/Tema4/HeroMain.java
@@ -6,28 +6,30 @@ public class HeroMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        final int POTIONFIND = 10;
-        final int REST = 1000;
         int wave = 0;
         Hero heroe = new Hero("Marga",1,30,30,0,30,0);
 
         System.out.println("Comienza el juego!! Sí tu personaje tiene estadisticas predeterminadas recuerda cambiarlo dentro del objeto Hero!");
         System.out.println("Tu personaje es " + heroe);
 
+        // Bucle principal
         while(heroe.getHealth()>0) {
             wave++;
             System.out.println("Llega la oleada número " + wave );
+
+            // Aquí se generan los enemigos
             int enemy = rand.nextInt(3) + 1;
             Hero[] enemies = new Hero[enemy];
-
             for (int i = 0; i < enemies.length;i++){
                 enemies[i] = new Hero("Goblin " + (i + 1), 1, 15, 15, 0, 7, 2);
             }
             System.out.println("Han aparecido " + enemy + " enemigos");
             boolean enemiesAlive = true;
 
+            // Bucle de rondas
             while (heroe.getHealth()>0 && enemiesAlive) {
 
+                // Sí los enemigos están vivos y sale el 10% huyen de la batalla
                 for (int i = 0; i < enemies.length; i++) {
                     if (enemies[i].getHealth() > 0) {
                         if (rand.nextInt(10) == 0) {
@@ -36,6 +38,8 @@ public class HeroMain {
                         }
                     }
                 }
+
+                // Ataque del jugador
                 boolean turnAttack = false;
                 for (int i = 0; i < enemies.length; i++) {
                     if (enemies[i].getHealth() > 0) {
@@ -47,6 +51,7 @@ public class HeroMain {
                 }
                 enemiesAlive = false;
 
+                // Ataque del Goblin
                 for (int i = 0; i < enemies.length; i++) {
                     if (enemies[i].getHealth() > 0) {
                         enemiesAlive = true;
@@ -54,29 +59,31 @@ public class HeroMain {
                         enemies[i].attack(heroe);
                     }
                 }
+                // Barra de vida super simple porque no me acuerdo de la otra
                 if (heroe.getHealth() > 0 && enemiesAlive) {
                     System.out.println("   [Tu Vida: " + heroe.getHealth() + "/" + heroe.getMaxHealth() + "]");
                 }
             }
+                // Sí se resuelven los bucles anteriores se revisa que el heroe siga vivo y se aplican las habilidades
                 if (heroe.getHealth() > 0) {
                     System.out.println("Horda eliminada");
 
+                    // Probabilidad de encontrar una poción
                     if (rand.nextInt(100) < 10) {
                         System.out.println("Has encontrado una poción entre los cadáveres");
                         heroe.drinkPotion();
                     }
 
+                    // Probabilidad de descansar
                     if (rand.nextInt(1000) == 0) {
                         System.out.println("Encuentras un lugar seguro y descansas");
                         heroe.rest();
                     }
                 }
             }
-
         System.out.println("GAME OVER");
         System.out.println("Has sobrevivido a " + (wave - 1) + " hordas completas.");
         System.out.println("Tu héroe murió luchando con honor.");
     }
 }
-
 
