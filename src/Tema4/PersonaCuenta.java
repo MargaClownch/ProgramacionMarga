020ab9502bf09202d790c2036aad4657a781a6d5
Marga <iulcaz@alu.edu.gva.es>
2026-02-02 10:35:06 +0100

    Ejercicios

--- src/Tema4/PersonaCuenta.java
+++ src/Tema4/PersonaCuenta.java
@@ -32,4 +32,30 @@ public class PersonaCuenta {
         cuentas[cuenta]++;
         }
     }
+
+    // Setters y getters
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public int[] getCuentas() {
+        return cuentas;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setCuentas(int[] cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
 }
