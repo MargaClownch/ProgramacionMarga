8a788cd4eae70d9c8279b94b77b7d53c73a5f181
Marga <iulcaz@alu.edu.gva.es>
2026-03-20 11:37:13 +0100

    Ejercicios

--- src/Tema7/EjercicioB1.java
+++ src/Tema7/EjercicioB1.java
@@ -1,9 +1,6 @@
 package Tema7;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 public class EjercicioB1 {
     public static void main(String[] args) {
