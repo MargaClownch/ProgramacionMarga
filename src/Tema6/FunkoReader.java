a740d52f972bcf0c65f890354f34128424ff6b8a
Marga <iulcaz@alu.edu.gva.es>
2026-04-16 12:15:20 +0200

    Ejercicios

--- src/Tema6/FunkoReader.java
+++ src/Tema6/FunkoReader.java
@@ -6,14 +6,55 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FunkoReader {
     public static void main(String[] args) {
+        cargarFunkos();
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        do {
+            showMenu();
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion !=0);
+    }
+    public static void showMenu(){
+        System.out.println("1. Añadir Funko");
+        System.out.println("2. Borrar Funko");
+        System.out.println("3. Mostrar todos los Funkos");
+        System.out.println("4. Mostrar el Funko más caro");
+        System.out.println("5. Mostrar la media de precio");
+        System.out.println("6. Mostrar funkos agrupados por modelos");
+        System.out.println("7. Mostrar los funkos del 2023");
+        System.out.println("0. Salir");
+
+    }
+    public static void cargarFunkos(){
         String rutaArchivo = "Resources/funkos.csv";
         ArrayList<Funko> listaFunkos = new ArrayList<>();
-
+        System.out.println("----------------------------------------------------------------");
         System.out.println("Sacando todos los funkos de su caja para que pierdan su valor :)");
-        System.out.println("------------------------------------");
+        System.out.println("----------------------------------------------------------------");
 
         try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
             String linea;
@@ -31,11 +72,14 @@ public class FunkoReader {
                     listaFunkos.add(nuevoFunko);
                 }
             }
-            System.out.println("Todos los funkos fuera de sus cajas!");
-            System.out.println("------------------------------------");
+
             for (Funko f : listaFunkos) {
                 System.out.println(f.toString());
+                System.out.println("--------------------------------------------------------------------------------------------------------------");
             }
+            System.out.println("------------------------------------");
+            System.out.println("Todos los funkos fuera de sus cajas!");
+            System.out.println("------------------------------------");
         }
         catch (IOException e){
             System.out.println("Error al leer el funko");
